03de60654b4e8adfb02dfa1899e1466c61ec042c
Sam Keir <samuelkeir98@gmail.com>
2018-05-20 22:16:52 -0400

    Very basic setup complete, implementing and testing simple commands.

---
+++ EggBot/Modules/Misc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace EggBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo()
+        {
+
+        }
+    }
+}
