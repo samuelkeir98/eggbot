3704009948ef0e5a419ba33d36741a3eb4d278de
Sam Keir <samuelkeir98@gmail.com>
2018-05-22 21:30:26 -0400

    Added pick command. Tested, works fine.

--- EggBot/Modules/Misc.cs
+++ EggBot/Modules/Misc.cs
@@ -11,9 +11,31 @@ namespace EggBot.Modules
     public class Misc : ModuleBase<SocketCommandContext>
     {
         [Command("echo")]
-        public async Task Echo()
+        public async Task Echo([Remainder]string msg)
         {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed message");
+            embed.WithDescription(msg);
+            embed.WithColor(new Color(0, 255, 0));
 
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        [Command("pick")]
+        public async Task PickOne([Remainder]string msg)
+        {
+            string[] options = msg.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choice for " + Context.User.Username);
+            embed.WithDescription(selection);
+            embed.WithColor(new Color(255, 255, 0));
+            embed.WithThumbnailUrl("http://www.clker.com/cliparts/f/O/o/N/A/N/brown-egg-hi.png");
+
+            await Context.Channel.SendMessageAsync("", false, embed);
         }
     }
 }
